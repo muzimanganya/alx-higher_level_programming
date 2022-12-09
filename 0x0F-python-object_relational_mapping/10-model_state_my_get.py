22c8399aa95054657302932b58c9fe1ad0f1613f
LAP Ltd <info@lapafrica.com>
2022-12-09 09:56:08 +0200

    changes for 2nd chance

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,37 +1,20 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-    A script that prints the State object with the name passed as an argument
-    from hbtn_0e_6_usa
-    Username, password, dbname and name to search
-    will be passed as arguments to the script.
-"""
-
-
 import sys
 from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    # create a session
+    Session = sessionmaker(bind=engine)
     session = Session()
-
-    # extract first state
-    states = session.query(State) \
-                    .filter(State.name == sys.argv[4]).one_or_none()
-
-    # print state.id
-    if states is None:
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
         print("Not found")
-    else:
-        print(states.id)
-
-    session.close()
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
 """
-    A script that deletes all State objects from hbtn_0e_6_usa that conatin
-    the letter a
-    Username, password and dbname wil be passed as arguments to the script.
-"""
-
-
 import sys
 from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                           sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    # create a session
+    Session = sessionmaker(bind=engine)
     session = Session()
-
-    # extract states with a in them
-    states = session.query(State).filter(State.name.ilike('%a%')).all()
-
-    # delete states
-    for state in states:
-        session.delete(state)
-
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
     session.commit()
-
-    session.close()
